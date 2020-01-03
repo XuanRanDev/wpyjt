4dcbb9beaedc64e10816cbd282c5cf9588986f6c
XuanRan0808 <3135419729@qq.com>
2020-01-03 19:51:17 +0800

    git第一次提交

--- app/src/main/java/com/XuanRan/wpyjt/MainActivity.java
+++ app/src/main/java/com/XuanRan/wpyjt/MainActivity.java
@@ -10,9 +10,8 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 		Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-
+        System.out.println("");
     }
 }
